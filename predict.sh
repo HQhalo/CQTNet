c0d9f8f8f27b3bf7b7759f0aa7cb815069fe8954
quanghoang <quang.hoang2@tiki.vn>
2021-12-10 13:15:00 +0700

    length

--- predict.sh
+++ predict.sh
@@ -1,1 +1,15 @@
-python demucs.py --in_path /data/full_song --out_separated_path /model/full_song_separated --out_vocal_path /model/vocals
+python spliter.py --in_path /data/full_song \
+  --out_separated_path /model/full_song_separated \
+  --out_vocal_path /model/vocals
+
+python gencqt.py --vocal_path /model/vocals \
+  --out_vocals_path /model/database/vocals \
+  --hum_path /data/hum \
+  --out_hum_path /model/database/hum
+
+mkdir /result
+
+python infer.py --load_model_path /content/drive/MyDrive/colabdrive/humming/fake_hum/45_0.905.pth \
+  --vocal_path  /model/database/vocals_npy
+  --hum_path  /model/database/hum_npy
+  --result_filename /result/submission.csv
